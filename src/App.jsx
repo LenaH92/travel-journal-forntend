77bb16f93aa28221e27521732ae4c31d84010a7e
Lena Cortes <lena.cortes@usal.es>
2024-12-26 18:56:53 +0100

    routes for last pages added

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ import AboutUsPage from './pages/aboutUsPage'
 import NotFoundPage from './pages/NotFoundPage'
 import HomePage from './pages/HomePage'
 import InstructionsPage from './pages/instructionsPage'
+import MyTripsPage from './pages/MyTripsPage'
+import AddNewTripPage from './pages/AddNewTripPage'
 
 function App() {
 
@@ -23,11 +25,11 @@ function App() {
 
         <Route path="/instructions" element={<InstructionsPage />} /> {/* I thought may be we can put a page to tell what they can do in the page? */}
 
-        <Route path="/my-trips" element={""} />
+        <Route path="/my-trips" element={<MyTripsPage />} />
 
         <Route path="/trip/:tripId" element={""} />
 
-        <Route path="/trip/new" element={""} />
+        <Route path="/trip/new" element={<AddNewTripPage />} />
 
         <Route path="/about-us" element={<AboutUsPage />} /> {/* I added an about us to put something about us, we can delete it if you dont want it */}
 
