7778f0432ae3a88e513aa1c5c108a3f354f937ff
miomate <mrspec@outlook.com>
2025-01-09 18:16:16 +0100

    now all images of a trip displayed, text message if none avaible

--- src/pages/TripDetailsPage.jsx
+++ src/pages/TripDetailsPage.jsx
@@ -12,7 +12,9 @@ const TripDetailsPage = ({ trips, handleDelete }) => {
   // Fetch the trip data based on the tripId from the URL
   useEffect(() => {
     if (tripsState && tripId) {
-      const foundTrip = tripsState.find((currentTrip) => String(currentTrip.id) === tripId);
+      const foundTrip = tripsState.find(
+        (currentTrip) => String(currentTrip.id) === tripId
+      );
       setTrip(foundTrip); // Set the trip state to the found trip
     }
   }, [tripId, tripsState]);
@@ -34,13 +36,16 @@ const TripDetailsPage = ({ trips, handleDelete }) => {
     try {
       // https://travel-journal-backend-t7fs.onrender.com/trips
       // const response = await fetch(`http://localhost:4000/trips/${updatedTrip.id}`, {
-      const response = await fetch(`${import.meta.env.VITE_API_URL}/trips/${updatedTrip.id}`, {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(updatedTrip),
-      });
+      const response = await fetch(
+        `${import.meta.env.VITE_API_URL}/trips/${updatedTrip.id}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(updatedTrip),
+        }
+      );
       const result = await response.json();
       if (response.ok) {
         // Update the trip in the state after successful update
@@ -50,10 +55,10 @@ const TripDetailsPage = ({ trips, handleDelete }) => {
         setTrip(updatedTrip); // Update the trip state after successful update
         setIsEditing(false); // Close the edit form after successful update
       } else {
-        console.error('Failed to update trip:', result);
+        console.error("Failed to update trip:", result);
       }
     } catch (error) {
-      console.error('Error updating trip:', error);
+      console.error("Error updating trip:", error);
     }
   };
 
@@ -76,7 +81,18 @@ const TripDetailsPage = ({ trips, handleDelete }) => {
           {trip.startDate ? ` from ${trip.startDate}` : ""}{" "}
           {trip.endDate ? ` to ${trip.endDate}` : ""}
         </h3>
-        <img src={trip.images[0]} alt="" />
+        <div className="imagesArrayDiv">
+          {/* if no images display p, if yes map through them, slice and display all */}
+          {trip.images.length === 0 ? (
+            <p>No images available for this trip.</p>
+          ) : (
+            trip.images
+              .slice(0, 2)
+              .map((image, index) => (
+                <img key={index} src={image} alt={`Trip image ${index + 1}`} />
+              ))
+          )}
+        </div>
       </div>
       <hr />
       <section className="descriptionDiv">
@@ -92,9 +108,7 @@ const TripDetailsPage = ({ trips, handleDelete }) => {
       )}
 
       {/* Show the Edit form only if isEditing is true */}
-      {isEditing && (
-        <Edit trip={trip} handleUpdate={handleUpdate} />
-      )}
+      {isEditing && <Edit trip={trip} handleUpdate={handleUpdate} />}
     </div>
   );
 };
